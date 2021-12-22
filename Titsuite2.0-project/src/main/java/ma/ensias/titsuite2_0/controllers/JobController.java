09d291818fd518a1964d8899a6abfd223c61b546
Amine JAMAI <mr.maj.2015@gmail.com>
2021-12-22 17:36:35 +0100

    New : add jobs by freelancer ID

--- Titsuite2.0-project/src/main/java/ma/ensias/titsuite2_0/controllers/JobController.java
+++ Titsuite2.0-project/src/main/java/ma/ensias/titsuite2_0/controllers/JobController.java
@@ -4,6 +4,7 @@ import ma.ensias.titsuite2_0.entities.Job;
 import ma.ensias.titsuite2_0.services.JobService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -17,5 +18,10 @@ public class JobController {
     List<Job> getMyJobs(){
         int idForTest =1; // This id should be returned by JWT
         return jobService.findMyJobs(idForTest);
+    }
+
+    @GetMapping("/jobs/freelancer/{id}")
+    List<Job> getJobsByFreelancer(@PathVariable int id){
+        return jobService.findMyJobs(id);
     }
 }
